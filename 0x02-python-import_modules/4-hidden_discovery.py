0079614781ad32e65f5a8754292df4d779617fb6
iLouis1 <rhiganakatugba@gmail.com>
2023-12-06 14:36:50 -0800

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if _name_ == "_main_":
 	from hidden_4 import *
 	allf = dir()
 	for i in range(0, len(allf)):
-		if allf[i][:2] != "_":
-			print("{:s}".format(allf[i]))
+            if allf[i][:2] != "_":
+                print("{:s}".format(allf[i]))
