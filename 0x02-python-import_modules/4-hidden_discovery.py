d0a1d678e7c8252d56b857aa5eb4523c4bb69e72
iLouis1 <rhiganakatugba@gmail.com>
2023-12-06 14:58:35 -0800

    4. Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-if _name_ == "_main_":
-        from hidden_4 import *
-        allf = dir()
-        for i in range(0, len(allf)):
-            if allf[i][:2] != "_":
-                print("{:s}".format(allf[i]))
+if __name__ == "__main__":
+
+    from hidden_4 import *
+    allf = dir()
+    for i in range(0, len(allf)):
+        if allf[i][:2] != "__":
+            print("{:s}".format(allf[i]))
