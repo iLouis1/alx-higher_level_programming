3ad70175deb7cfdff6954043e77158d485809336
iLouis1 <rhiganakatugba@gmail.com>
2024-03-28 10:37:15 -0700

    Task 3

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This script will send a POST request to the passed URL with the email as a parameter,
-and displays the body of the response (decoded in utf-8)
+This script will send a POST request to the passed URL with the email as
+a parameter, and display the body of the response (decoded in utf-8)
 """
 
 if __name__ == '__main__':
